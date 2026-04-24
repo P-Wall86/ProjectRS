9fb6fb44aef544291373beff3982d9f228f156f9
P-Wall86 <sisterwall@gmail.com>
2026-04-24 19:32:16 -0300

    Style Fixes

--- scripts/data/presidency.js
+++ scripts/data/presidency.js
@@ -2,16 +2,16 @@ export const presidency = [
     {
         name: "Pamela Pared",
         role: "Presidenta",
-        image: "images/Wall.webp"
+        image: "images/Wall.webp",
     },
     {
         name: "Elizabeth Espínola",
         role: "1ra Consejera",
-        image: "images/Eli.webp"
+        image: "images/Eli.webp",
     },
     {
         name: "Rosina Agüero",
         role: "2da Consejera",
-        image: "images/Ro.webp"
+        image: "images/Ro.webp",
     }
 ];
